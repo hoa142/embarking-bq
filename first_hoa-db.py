18bec9e1689d5562630f13cfce607213cd625c56
Hoa Tran <hoa.tran@anfin.vn>
2022-06-26 19:57:42 +0700

    Update dbt first-try

--- first_hoa-db.py
+++ first_hoa-db.py
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine
 from google.cloud import bigquery
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
@@ -14,7 +15,6 @@ SELECT_FILM = "SELECT * FROM public.film;"
 
 
 connection = psycopg2.connect(os.environ["DATABASE_URL"])
-
 
 
 # with connection:
@@ -52,53 +52,38 @@ engine = create_engine(engine_string)
 # job.result()
 
 
-def load_table_to_bq(data_set: str, table_name: str, write_mode="WRITE_EMPTY"):  # If the table already exists and contains data, a 'duplicate error is returned in the job result.
+def load_table_to_bq(
+    data_set: str, table_name: str, write_mode="WRITE_EMPTY"
+):  # If the table already exists and contains data, a 'duplicate error is returned in the job result.
     df = pd.read_sql_table(table_name, engine)
     client = bigquery.Client()
     table_id = f"{data_set}.{table_name}"
     job_config = bigquery.LoadJobConfig()
     job_config.write_disposition = f"{write_mode}"
 
-
-    job = client.load_table_from_dataframe(
-        df, table_id, job_config=job_config
-    )
+    job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
 
     # Wait for the load job to complete.
     return job.result()
 
-#print(load_table_to_bq('first_trial', 'category')) # WRITE_APPEND or WRITE_TRUNCATE
 
+# print(load_table_to_bq('first_trial', 'category')) # WRITE_APPEND or WRITE_TRUNCATE
 
 
 def add_data(data_set: str, table_name: str, write_mode="WRITE_APPEND"):
-    df = pd.read_sql_query("""select * from film_category where last_update >= now() - interval '1 hour'""", engine)
+    df = pd.read_sql_query(
+        """select * from film_category where last_update >= now() - interval '1 hour'""",
+        engine,
+    )
     client = bigquery.Client()
     table_id = f"{data_set}.{table_name}"
     job_config = bigquery.LoadJobConfig()
     job_config.write_disposition = f"{write_mode}"
 
-    job = client.load_table_from_dataframe(
-        df, table_id, job_config=job_config
-    )
+    job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
 
     # Wait for the load job to complete.
     return job.result()
 
-print(add_data('first_trial', 'film_category', 'WRITE_APPEND'))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(add_data("first_trial", "film_category", "WRITE_APPEND"))
