096c4a68db3ef8e8703d5291f51bc3caf79cc2c1
Hoa Tran <hoa.tran@anfin.vn>
2022-06-26 19:46:59 +0700

    Update bq_main

--- bq_main.py
+++ bq_main.py
@@ -1,3 +1,7 @@
 from bq_libs import load_pg_table_to_bq, append_pg_table_to_bq
 
-print(append_pg_table_to_bq("public", "film", "first_trial", "film", "last_update", "1 hour"))
+print(
+    append_pg_table_to_bq(
+        "public", "film", "first_trial", "film", "last_update", "1 hour"
+    )
+)
